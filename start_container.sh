912a84929302ff2951309f9fdd80c003c5c3490d
riyaserjan <riya.serjan@gmail.com>
2024-05-20 16:54:36 -0400

    Second commit corrected docker image pull and run commands

--- start_container.sh
+++ start_container.sh
@@ -3,8 +3,8 @@ set -e
 
 # Pull the Docker image from Docker Hub
 echo "docker pull"
-docker pull riyaserjan/pythonapp
+docker pull riyaserjan/pythonapp:latest
 
 # Run the Docker image as a container
 echo "docker run"
-docker run -d -p 5000:5000 riyaserjan/pythonapp
+docker run -d -p 5000:5000 riyaserjan/pythonapp:latest
